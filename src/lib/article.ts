4bc6652695f612ae90a3d183613150490653d18b
Samuel Kuria <iamsamuelkuria@gmail.com>
2025-10-21 12:38:07 +0300

    updated

--- src/lib/article.ts
+++ src/lib/article.ts
@@ -2,7 +2,7 @@ import { Readability } from "@mozilla/readability";
 import { parseHTML } from "linkedom";
 import { ArticleSummary } from "../types";
 import DOMPurify from "dompurify";
-import summarize from "./summarize";
+//import summarize from "./summarize";
 
 export async function getArticleAndSummary(options: {
   articlesKV: KVNamespace;
@@ -37,21 +37,27 @@ export async function getArticleAndSummary(options: {
   });
 
   let reader: Readability | null = null;
+
   try {
     reader = new Readability(document);
   } catch (error) {
     console.error("Readability parsing error:", (error as Error).message);
   }
 
-  const article = reader!.parse();
-
   result = {
     article: null,
     summary: null,
   };
 
+  if (!reader) {
+    await options.articlesKV.put(options.url, JSON.stringify(result));
+    return result;
+  }
+
+  const article = reader?.parse();
+
   if (article?.content) {
-    const { window } = parseHTML(html);
+    const { window } = parseHTML("");
     const purify = DOMPurify(window);
     const cleanArticle = purify.sanitize(article.content);
     const cleanExcerpt = purify.sanitize(article.excerpt);
@@ -64,11 +70,6 @@ export async function getArticleAndSummary(options: {
       summary: cleanExcerpt,
       //summary,
     };
-  }
-
-  if (!reader) {
-    await options.articlesKV.put(options.url, JSON.stringify(result));
-    return result;
   }
 
   await options.articlesKV.put(options.url, JSON.stringify(result));
